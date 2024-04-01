479a47e9520ef41dacc16b58f783166438df6e69
TheLLiRRiKminD <kill2002@mail.ru>
2024-04-01 18:10:10 +0300

    Docker, rev.3

--- config/settings.py
+++ config/settings.py
@@ -154,10 +154,10 @@ CORS_ALLOWED_ORIGINS = [
 # Настройки для Celery
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://redis:6379/0'  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0' #redis
 
 # Флаг отслеживания выполнения задач
 CELERY_TASK_TRACK_STARTED = True
